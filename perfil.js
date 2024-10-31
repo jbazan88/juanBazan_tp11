ed3fe28ad461bb5bdea050ca1fe6c708cfdf0bcc
Juan Bazan <juan.bazan88@yahoo.com>
2024-10-31 19:21:33 -0300

    Desafio 1

---
+++ perfil.js
@@ -0,0 +1,15 @@
+let perfil = "invitado";
+
+if (perfil === "") {
+    console.log("Debe especificar el perfil del usuario");
+} else if (perfil.toLowerCase() === "administrador") {
+    console.log("Usted tiene todos los privilegios de uso del sistema");
+} else if (perfil.toLowerCase() === "asistente") {
+    console.log("Usted sólo tiene permisos de registrar, modificar y consultar datos");
+} else if (perfil.toLowerCase() === "invitado") {
+    console.log("Usted sólo tiene permisos de consultar datos");
+
+} else {
+    console.log("Debe especificar un perfil válido");
+
+}
